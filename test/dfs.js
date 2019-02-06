3ba78b4617bb9b796d7230d768aaf6d967301ec5
ajlopez <webmaster@ajlopez.com>
2019-02-06 11:14:52 -0300

    Create data frame

---
+++ test/dfs.js
@@ -0,0 +1,11 @@
+
+const dfs = require('../lib/dfs');
+
+exports['create data frame from data'] = function (test) {
+    const df = dfs.df([ [ 'country', 'year', 'pop' ], [ 'Afganistan', 1980, 10200300 ]]);
+    
+    test.ok(df);
+    test.equal(typeof df, 'object');
+    test.deepEqual(df.shape(), [ 1, 3 ]);
+};
+
