f7a64ff6a6ae7474665ff691d1d52f1623549667
ajlopez <webmaster@ajlopez.com>
2019-02-16 10:32:48 -0300

    Rename method df to dataframe

--- test/dfs.js
+++ test/dfs.js
@@ -2,7 +2,7 @@
 const dfs = require('../lib/dfs');
 
 exports['create data frame from columns and data'] = function (test) {
-    const df = dfs.df([ [ 'Afganistan', 1980, 10200300 ] ], [ 'country', 'year', 'pop' ]);
+    const df = dfs.dataframe([ [ 'Afganistan', 1980, 10200300 ] ], [ 'country', 'year', 'pop' ]);
     
     test.ok(df);
     test.equal(typeof df, 'object');
@@ -10,13 +10,13 @@ exports['create data frame from columns and data'] = function (test) {
 };
 
 exports['get column names'] = function (test) {
-    const df = dfs.df([ [ 'Afganistan', 1980, 10200300 ] ], [ 'country', 'year', 'pop' ]);
+    const df = dfs.dataframe([ [ 'Afganistan', 1980, 10200300 ] ], [ 'country', 'year', 'pop' ]);
     
     test.deepEqual(df.columns(), [ 'country', 'year', 'pop' ]);
 };
 
 exports['get index labels'] = function (test) {
-    const df = dfs.df([ [ 'Afganistan', 1980, 10200300 ] ], [ 'country', 'year', 'pop' ]);
+    const df = dfs.dataframe([ [ 'Afganistan', 1980, 10200300 ] ], [ 'country', 'year', 'pop' ]);
     
     test.deepEqual(df.index(), [ 0 ]);
 };
